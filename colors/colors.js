1a581b1722b58ab53aa7c5b6d877e35f5745fbfa
mkrensing <martin@krensing.de>
2024-10-03 19:53:33 +0200

    colors.js

--- colors/colors.js
+++ colors/colors.js
@@ -2,18 +2,18 @@ const  colors = [
     { name: "Red", shades: ["#ffcccc", "#ff9999", "#ff6666", "#ff3333", "#cc0000"] },
     { name: "Blue", shades: ["#cce6ff", "#99ccff", "#66b3ff", "#3399ff", "#0066cc"] },
     { name: "Green", shades: ["#ccffcc", "#99ff99", "#66ff66", "#33cc33", "#009900"] },
-    { name: "Yellow", shades: ["#ffffcc", "#ffff99", "#ffff66", "#ffff33", "#cccc00"] },
-    { name: "Purple", shades: ["#f2ccff", "#e6b3ff", "#d966ff", "#cc33ff", "#9900cc"] },
     { name: "Orange", shades: ["#ffe6cc", "#ffcc99", "#ffb366", "#ff9933", "#cc6600"] },
+    { name: "Purple", shades: ["#f2ccff", "#e6b3ff", "#d966ff", "#cc33ff", "#9900cc"] },
     { name: "Brown", shades: ["#e6ccb3", "#d9b38c", "#cc9966", "#b37700", "#663300"] },
     { name: "Gray", shades: ["#f2f2f2", "#d9d9d9", "#bfbfbf", "#808080", "#404040"] },
-    { name: "Lime", shades: ["#e6ffcc", "#ccff99", "#b3ff66", "#99ff33", "#66cc00"] },
     { name: "Indigo", shades: ["#e6e6ff", "#ccccff", "#b3b3ff", "#9999ff", "#6666cc"] },
     { name: "Olive", shades: ["#f2f2cc", "#e6e699", "#d9d966", "#cccc33", "#999900"] },
     { name: "Pink", shades: ["#ffd9e6", "#ffb3cc", "#ff8cb3", "#ff6699", "#cc3366"] },
     { name: "Teal", shades: ["#ccffff", "#99ffff", "#66ffff", "#33cccc", "#009999"] },
     { name: "Mint", shades: ["#e6fff2", "#ccffe6", "#99ffcc", "#66ffb3", "#33cc99"] },
-    { name: "Gold", shades: ["#fff5cc", "#ffeb99", "#ffe066", "#ffd633", "#cca300"] }
+    { name: "Gold", shades: ["#fff5cc", "#ffeb99", "#ffe066", "#ffd633", "#cca300"] },
+    { name: "Lime", shades: ["#e6ffcc", "#ccff99", "#b3ff66", "#99ff33", "#66cc00"] },    
+    { name: "Yellow", shades: ["#ffffcc", "#ffff99", "#ffff66", "#ffff33", "#cccc00"] }
   ];
 
 export function getColors() {
